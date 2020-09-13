ac39919f2cff5b496a1176c870ea38efbaa8169a
sklukin <sklukin@yandex.ru>
2020-09-14 01:37:47 +0300

    make build

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Mojolicious::Plugin::Gallery - Blah blah blah
+
+# SYNOPSIS
+
+    use Mojolicious::Plugin::Gallery;
+
+# DESCRIPTION
+
+Mojolicious::Plugin::Gallery is
+
+# AUTHOR
+
+sklukin <sklukin@yandex.ru>
+
+# COPYRIGHT
+
+Copyright 2020- sklukin
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Mojolicious/Plugin/Gallery.pm
@@ -0,0 +1,39 @@
+package Mojolicious::Plugin::Gallery;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::Plugin::Gallery - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Mojolicious::Plugin::Gallery;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::Gallery is
+
+=head1 AUTHOR
+
+sklukin E<lt>sklukin@yandex.ruE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2020- sklukin
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
